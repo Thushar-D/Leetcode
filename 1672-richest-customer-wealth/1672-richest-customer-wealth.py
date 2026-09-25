dd7eb0d71cfca0d0f950482eddab155a3e4e528c
Thushar <thushar3d@gmail.com>
2026-09-25 22:55:03 +0530

    Time: 0 ms (100%), Space: 12.5 MB (28.23%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def maximumWealth(self, accounts):
+        richest = 0
+
+        for customer in accounts:
+            wealth = sum(customer)
+            richest = max(richest, wealth)
+
+        return richest
